c445b8883c0cb9901e9a681f5b83398520a75775
bryce <bryce.hoehn@mailbox.org>
2023-11-04 23:53:04 -0400

    #7

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import socket
 import qrcode
 import os
-import threading
 import re
 from yt_dlp import YoutubeDL
 from flask import Flask, render_template, request, redirect, flash, url_for, send_from_directory
 from flask_httpauth import HTTPDigestAuth
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -25,6 +24,20 @@ def get_pw(username):
     if username in users:
         return users.get(username)
     return None
+
+# ---------------- Functions ----------------
+
+def search_youtube(yt_search):
+    ydl_opts = {
+        'format': 'best',   # You can specify the format you want
+        'extract_flat': True,
+        'extract_no_playlists': True
+    }
+
+    with YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(yt_search, download=False)
+    
+    return result
 
 # ---------------- Mobile Routes ----------------
 
@@ -46,19 +59,7 @@ def search():
             num_results = 5
             yt_search = f'ytsearch{num_results}:"{song} karaoke"'
             result = search_youtube(yt_search)
-
-        if 'selection' in request.form:
-            video_id = request.form['selection']
-            video_metadata = search_youtube(video_id)
-            video_title = re.sub(r'\s*\(.*\)', '', video_metadata['title'])
-
-            if not os.path.isfile(f'{{ video_id }}.mp4'):
-                download_video(video_id)
-
-            song_queue.update({video_id: video_title})
-            socketio.emit('play_video')
             
-
     return render_template("search.html", active="search", result=result)
 
 
@@ -90,46 +91,44 @@ def play_video():
 def serve_video(filename):
     return send_from_directory('/songs', filename)
 
-@socketio.on('connect')
-def connect_event():
-    print('Client connected')
+@socketio.on('start_download')
+def start_download(data):
+    video_id = data['video_id']
+    video_metadata = search_youtube(video_id)
+    video_title = re.sub(r'\s*\(.*\)', '', video_metadata['title'])
 
-@socketio.on('player_restart')
+    song_queue.update({video_id: video_title})
+
+    if not os.path.isfile(f'./songs/{video_id}.mp4'):
+        ydl_opts = {
+            'outtmpl': f'/songs/{video_id}.mp4',
+            'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download(video_id)
+        
+    socketio.emit('play_video', namespace='/tv')
+
+@socketio.on('connect', namespace='/')
+def mobile_connect():
+    print("Mobile client connected")
+
+@socketio.on('connect', namespace='/tv')
+def mobile_connect():
+    print("TV client connected")
+
+@socketio.on('player_restart', namespace='/')
 def player_restart():
-    socketio.emit('player_restart')
+    socketio.emit('player_restart', namespace='/tv')
 
-@socketio.on('player_pause')
+@socketio.on('player_pause', namespace='/')
 def player_pause():
-    socketio.emit('player_pause')
+    socketio.emit('player_pause', namespace='/tv')
 
-@socketio.on('player_skip')
+@socketio.on('player_skip', namespace='/')
 def player_skip():
-    socketio.emit('player_skip')
-
-
-# ---------------- Functions ----------------
-
-def search_youtube(yt_search):
-    ydl_opts = {
-        'format': 'best',   # You can specify the format you want
-        'extract_flat': True,
-        'extract_no_playlists': True
-    }
-
-    with YoutubeDL(ydl_opts) as ydl:
-        result = ydl.extract_info(yt_search, download=False)
-    
-    return result
-
-def download_video(song):
-    ydl_opts = {
-        'outtmpl': f'/songs/{song}.mp4',
-        'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"
-    }
-
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download(song)
-
+    socketio.emit('player_skip', namespace='/tv')
 
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=8080)
